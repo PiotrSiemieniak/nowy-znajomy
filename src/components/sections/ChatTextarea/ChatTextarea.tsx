e8ea8f6213cb3f2e11740e1af03edac4b993f36e
Piotr Siemieniak <siemieniak.piotr1@gmail.com>
2025-04-15 00:20:42 +0200

    Button and Textarea has been created in ui

---
+++ src/components/sections/ChatTextarea/ChatTextarea.tsx
@@ -0,0 +1,7 @@
+export function ChatTextarea() {
+  return (
+    <div className="bg-black/25 backdrop-blur-md flex justify-center items-center w-full min-h-10 h-fit absolute bottom-20 left-0 px-2 rounded-t-xl">
+      <div className="w-full">Test</div>
+    </div>
+  );
+}
