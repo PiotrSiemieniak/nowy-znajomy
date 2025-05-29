0d4df2865a4ac5d2cfe165a9ab96123f58446ffc
Piotr Siemieniak <siemieniak.piotr1@gmail.com>
2025-05-30 00:12:30 +0200

    ThemeSwitcher content hotfix

--- src/components/specific/ThemeSwitcher/ThemeSwitcher.tsx
+++ src/components/specific/ThemeSwitcher/ThemeSwitcher.tsx
@@ -10,52 +10,41 @@ export function ThemeSwitcher({ className }: { className?: string }) {
   const theme = localStorage.theme;
 
   return (
-    <div className={cn("grid gap-4", className)}>
-      {/* <h4 className="font-medium leading-none">Niezalogowany</h4>
-          <div className="bg-muted h-px w-full" /> */}
-      <div className="flex space-x-4">
-        <Button size={"sm"} className="flex-1">
-          Zaloguj
-        </Button>
-        <Button size={"sm"} className="flex-1">
-          Zarejestruj
-        </Button>
-      </div>
-      <div className="bg-muted h-px w-full" />
-      <div>
-        <h5 className="font-medium">Opcje</h5>
-        <h6>Tryb UI</h6>
+    <>
+      <h6>Tryb UI</h6>
 
-        <ToggleGroup
-          className="w-full border bg-slate-200 dark:bg-slate-700"
-          value={theme}
-          onValueChange={setTheme}
-          defaultValue={theme}
-          type="single"
+      <ToggleGroup
+        className={cn(
+          "w-full border bg-slate-200 dark:bg-slate-700",
+          className
+        )}
+        value={theme}
+        onValueChange={setTheme}
+        defaultValue={theme}
+        type="single"
+      >
+        <ToggleGroupItem
+          className="flex-1 hover:bg-accent/75"
+          value={"light"}
+          aria-label={"light theme"}
         >
-          <ToggleGroupItem
-            className="flex-1 hover:bg-accent/75"
-            value={"light"}
-            aria-label={"light theme"}
-          >
-            <Sun />
-          </ToggleGroupItem>
-          <ToggleGroupItem
-            className="flex-1 hover:bg-accent/75"
-            value={"dark"}
-            aria-label={"dark theme"}
-          >
-            <Moon />
-          </ToggleGroupItem>
-          <ToggleGroupItem
-            className="flex-1 hover:bg-accent/75"
-            value={"system"}
-            aria-label="system theme"
-          >
-            <ComputerIcon />
-          </ToggleGroupItem>
-        </ToggleGroup>
-      </div>
-    </div>
+          <Sun />
+        </ToggleGroupItem>
+        <ToggleGroupItem
+          className="flex-1 hover:bg-accent/75"
+          value={"dark"}
+          aria-label={"dark theme"}
+        >
+          <Moon />
+        </ToggleGroupItem>
+        <ToggleGroupItem
+          className="flex-1 hover:bg-accent/75"
+          value={"system"}
+          aria-label="system theme"
+        >
+          <ComputerIcon />
+        </ToggleGroupItem>
+      </ToggleGroup>
+    </>
   );
 }
