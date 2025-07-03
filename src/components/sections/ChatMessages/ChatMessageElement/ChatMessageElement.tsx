8855d15a18cfd2d8c2d53c9bc6c36cb58db0d0de
Piotr Siemieniak <siemieniak.piotr1@gmail.com>
2025-07-03 22:55:41 +0200

    Breaking line has been added to ChatMessageElement div style

--- src/components/sections/ChatMessages/ChatMessageElement/ChatMessageElement.tsx
+++ src/components/sections/ChatMessages/ChatMessageElement/ChatMessageElement.tsx
@@ -1,6 +1,3 @@
-"use client";
-
-import { useChatState } from "@/components/providers/ChatProvider";
 import { cn } from "@/lib/utils";
 
 export function ChatMessageElement({
@@ -14,12 +11,15 @@ export function ChatMessageElement({
 }) {
   return (
     <div
-      className={cn("text-xs bg-card/75 backdrop-blur-md p-2 w-fit max-w-96", {
-        "ml-auto rounded-l-xl rounded-tr-xl bg-card/25": isItMe,
-        "mr-auto rounded-r-xl rounded-bl-xl bg-card/55": !isItMe,
-        "mt-1": isPrevMessageSameAuthor,
-        "mt-4": !isPrevMessageSameAuthor,
-      })}
+      className={cn(
+        "text-xs bg-card/75 backdrop-blur-md p-2 w-fit max-w-96 whitespace-pre-line",
+        {
+          "ml-auto rounded-l-xl rounded-tr-xl bg-card/25": isItMe,
+          "mr-auto rounded-r-xl rounded-bl-xl bg-card/55": !isItMe,
+          "mt-1": isPrevMessageSameAuthor,
+          "mt-4": !isPrevMessageSameAuthor,
+        }
+      )}
     >
       {text}
     </div>
