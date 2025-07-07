771ae7ccd7466c08d58e8c663260f32fba487362
Piotr Siemieniak <siemieniak.piotr1@gmail.com>
2025-07-07 20:19:54 +0200

    Removed unused waiting prop

--- src/app/chat/page.tsx
+++ src/app/chat/page.tsx
@@ -1,13 +1,5 @@
 import ChatPage from "@/components/views/ChatPage/ChatPage";
 
-export default async function Page({
-  searchParams,
-}: {
-  searchParams: Promise<{
-    [key: string]: string | undefined;
-  }>;
-}) {
-  const { id } = await searchParams;
-
-  return <ChatPage chatId={id} />;
+export default function Page() {
+  return <ChatPage />;
 }
