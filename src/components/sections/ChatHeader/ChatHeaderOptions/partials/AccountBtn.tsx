92201058c6c76f73c478bbf17189384c2ea68b19
Piotr Siemieniak <siemieniak.piotr1@gmail.com>
2025-07-10 15:27:15 +0200

    feat: add dropdown menu component and integrate with account options

--- src/components/sections/ChatHeader/ChatHeaderOptions/partials/AccountBtn.tsx
+++ src/components/sections/ChatHeader/ChatHeaderOptions/partials/AccountBtn.tsx
@@ -9,10 +9,14 @@ import { cn } from "@/lib/utils";
 import { LoginContent } from "@/components/specific/LoginContent/LoginContent";
 import { UserCog } from "lucide-react";
 import { AccountTrigger } from "@/components/specific/AccountTrigger";
-import { Separator } from "@/components/ui/Separator";
 import { RegisterContent } from "@/components/specific/RegisterContent";
+import { useSession } from "next-auth/react";
 
 export function AccountBtn() {
+  const { data: session, status } = useSession();
+  const isLoading = status === "loading";
+  const isAuthenticated = status === "authenticated" && session?.user;
+
   return (
     <Popover>
       <PopoverTrigger asChild>
@@ -20,15 +24,21 @@ export function AccountBtn() {
           Konto <UserCog className="ml-1" />
         </Button>
       </PopoverTrigger>
-      <PopoverContent className="bg-card/75 w-80 backdrop-blur-lg">
+      <PopoverContent className="bg-cardGlass w-80 backdrop-blur-lg">
         <div className={cn("grid gap-4")}>
-          <div className="flex space-x-4">
-            <AccountTrigger />
-            <LoginContent />
-            <RegisterContent />
+          <div className="flex space-x-4 min-h-[56px] items-center justify-center">
+            {isLoading ? (
+              <div className="w-full flex justify-center items-center animate-pulse bg-muted rounded-xl h-[72px]"></div>
+            ) : isAuthenticated ? (
+              <AccountTrigger />
+            ) : (
+              <>
+                <LoginContent />
+                <RegisterContent />
+              </>
+            )}
           </div>
-          <Separator />
-          <div className="space-y-2">
+          <div className="space-y-2 px-2">
             <h5 className="font-medium">Opcje</h5>
             <ThemeSwitcher />
             <h6>Automatyczne połączenia</h6>
--- src/components/specific/AccountTrigger/AccountTrigger.tsx
+++ src/components/specific/AccountTrigger/AccountTrigger.tsx
@@ -1,38 +1,100 @@
+import { Button } from "@/components/ui/Button";
+import {
+  DropdownMenuItem,
+  DropdownMenuSeparator,
+  DropdownMenuShortcut,
+  DropdownMenuSub,
+} from "@/components/ui/DropdownMenu";
+import { Typography } from "@/components/ui/Typography";
 import { cn } from "@/lib/utils";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuGroup,
+  DropdownMenuLabel,
+  DropdownMenuPortal,
+  DropdownMenuSubContent,
+  DropdownMenuSubTrigger,
+  DropdownMenuTrigger,
+} from "@/components/ui/DropdownMenu";
+import { MoreVertical } from "lucide-react";
 import { useSession } from "next-auth/react";
 import Image from "next/image";
 
 export function AccountTrigger({ className }: { className?: string }) {
   const { data: session } = useSession();
-  console.log("AccountTrigger session:", session);
 
   if (!session) return null;
 
   const { user } = session;
 
   return (
-    <div
-      className={cn(
-        "rounded-xl flex gap-2 p-4 w-full max-w-96 bg-card hover:bg-accent transition-colors",
-        className
-      )}
-    >
-      <div className="relative size-10 min-w-10 rounded-full overflow-hidden bg-black">
-        <Image
-          src={user?.image || ""}
-          alt="Avatar"
-          className="object-cover"
-          fill
-        />
-      </div>
-      <div className="my-auto">
-        <p className="text-sm text-foreground font-medium">
-          Witaj, {user?.name}
-        </p>
-        <p className="text-xs text-muted-foreground">
-          Kliknij, aby przejść do ustawień konta
-        </p>
-      </div>
-    </div>
+    <DropdownMenu>
+      <DropdownMenuTrigger asChild>
+        <Button
+          variant={"ghost"}
+          className={cn(
+            "rounded-xl flex gap-2 p-2 w-full max-w-96 h-fit hover:bg-accent transition-colors",
+            className
+          )}
+        >
+          <div className="relative h-full aspect-square min-w-10 rounded-2xl overflow-hidden bg-muted">
+            {user?.image && (
+              <Image
+                src={user?.image || ""}
+                alt="Avatar"
+                className="object-cover"
+                fill
+              />
+            )}
+          </div>
+          <div className="my-auto">
+            <Typography variant={"small"} className="text-left">
+              {user?.name}
+            </Typography>
+            <Typography variant={"muted"}>
+              {user?.email || "Brak adresu e-mail"}
+            </Typography>
+          </div>
+          <Button
+            size={"icon"}
+            variant={"ghost"}
+            className="ml-auto p-0  h-8 w-8"
+          >
+            <MoreVertical />
+          </Button>
+        </Button>
+      </DropdownMenuTrigger>
+      <DropdownMenuContent className="w-56" align="start">
+        <DropdownMenuLabel>Moje konto</DropdownMenuLabel>
+        <DropdownMenuGroup>
+          <DropdownMenuItem>
+            Edytuj
+            <DropdownMenuShortcut>⇧⌘P</DropdownMenuShortcut>
+          </DropdownMenuItem>
+          <DropdownMenuItem>
+            Znajomi
+            <DropdownMenuShortcut>⌘B</DropdownMenuShortcut>
+          </DropdownMenuItem>
+          <DropdownMenuItem>
+            Historia wiadomości
+            <DropdownMenuShortcut>⌘S</DropdownMenuShortcut>
+          </DropdownMenuItem>
+          <DropdownMenuItem>
+            Ulepsz do Pro
+            <DropdownMenuShortcut>⌘S</DropdownMenuShortcut>
+          </DropdownMenuItem>
+        </DropdownMenuGroup>
+        <DropdownMenuSeparator />
+        <DropdownMenuItem>Udostępnij aplikację</DropdownMenuItem>
+        <DropdownMenuItem>Pobierz aplikację</DropdownMenuItem>
+        <DropdownMenuItem disabled>API</DropdownMenuItem>
+        <DropdownMenuSeparator />
+        <DropdownMenuItem className="hover:bg-red-200 dark:bg-red-900 hover:dark:bg-red-800">
+          Log out
+          <DropdownMenuShortcut>⇧⌘Q</DropdownMenuShortcut>
+        </DropdownMenuItem>
+      </DropdownMenuContent>
+    </DropdownMenu>
   );
 }
